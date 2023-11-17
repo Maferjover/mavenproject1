06676735ac6b91e6c563c0b4f14ea8f65911de93
maferjover <nanda.jover@gmail.com>
2023-11-17 16:39:11 +0100

    ejecutar

---
+++ OneDrive/Documentos/NetBeansProjects/proyectonew/src/main/java/com/mycompany/proyectonew/Proyectonew.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectonew;
+
+/**
+ *
+ * @author nanda
+ */
+public class Proyectonew {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
