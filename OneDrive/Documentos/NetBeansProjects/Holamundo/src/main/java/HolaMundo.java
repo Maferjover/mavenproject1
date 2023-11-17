54453a34abcdedaa7d1d06acd2d4d7c470691108
maferjover <nanda.jover@gmail.com>
2023-11-17 16:03:59 +0100

    ejecutar

--- OneDrive/Documentos/NetBeansProjects/Holamundo/src/main/java/HolaMundo.java
+++ OneDrive/Documentos/NetBeansProjects/Holamundo/src/main/java/HolaMundo.java
@@ -14,6 +14,7 @@ import static java.time.Clock.system;
 public class HolaMundo {
     public static void main(String[] args){
         System.out.print("Hola,Mundo");
+        //verificar
     }
     
 }
